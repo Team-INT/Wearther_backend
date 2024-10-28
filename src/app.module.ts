2cd0f03a92334e935d3a2555a3b648c6e9db3bdf
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-10-28 13:03:11 +0900

    환경변수 세팅, docker connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,31 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 
+// TypeORM
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// entity
+import { PostModel } from './posts/entities/post.entity';
+import { UserModel } from './users/entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
-  imports: [AuthModule, UsersModule, PostsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [PostModel, UserModel],
+      synchronize: true,
+    }),
+    AuthModule,
+    UsersModule,
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
