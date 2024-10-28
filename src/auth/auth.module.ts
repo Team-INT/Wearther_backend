e3a80b0ba91d8a7d68ba38cb9722b2d021f879cc
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-10-28 14:24:01 +0900

    auth controller, service
    jwt 토큰 로직 작성 중

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+
+// auth
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
+// users
+import { UsersModule } from 'src/users/users.module';
+
 @Module({
+  imports: [JwtModule.register({}), UsersModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { JwtService } from '@nestjs/jwt';
+
+// users
+import { UserModel } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly usersService: UsersService,
+  ) {}
 
-  findAll() {
-    return `This action returns all auth`;
-  }
+  signToken(user: Pick<UserModel, 'email' | 'id'>, isRefreshToken: boolean) {
+    /**
+     * Payload에 들어갈 정보
+     * 1) email
+     * 2) sub -> id
+     * 3) type: 'access' | 'refresh'
+     *
+     */
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
+    const payload = {
+      email: user.email,
+      sub: user.id,
+      type: isRefreshToken ? 'refresh' : 'access',
+    };
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+    return this.jwtService.sign(payload, {
+      secret: process.env.JWT_SECRET,
+      expiresIn: isRefreshToken
+        ? parseInt(process.env.REFRESH_TOKEN_TIME) * 60
+        : parseInt(process.env.ACCESS_TOKEN_TIME) * 60,
+    });
   }
 }
