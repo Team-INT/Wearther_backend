f3ffc1687a81f54594c11ca1030707079c3a88ec
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-10-28 15:10:33 +0900

    users 서비스 작업 중

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -34,4 +34,41 @@ export class AuthService {
         : parseInt(process.env.ACCESS_TOKEN_TIME) * 60,
     });
   }
+
+  loginUser(user: Pick<UserModel, 'email' | 'id'>) {
+    return {
+      accessToken: this.signToken(user, false),
+      refreshToken: this.signToken(user, true),
+    };
+  }
+
+  /**
+   * 1) 사용자가 존재하는지 확인(email)
+   * 2) 비밀번호가 맞는지 확인(pasword - hash키 비교)
+   * 3) 모두 통과되는 사용자 정보 반환
+   */
+
+  async authenticateWithEmailAndPassword(
+    user: Pick<UserModel, 'email' | 'password'>,
+  ) {}
+
+  async loginWithEmail() {}
+
+  async registerWithEmail() {}
+
+  /**
+   *
+   * Header로 부터 토큰을 받을 때
+   * {authorization: 'Basic {token}'}
+   * {authorization: 'Bearer {token}'}
+   */
+
+  extractTokenFromHeader() {}
+  /**
+   * Basic adsklfjadlsfjkladsjklf -> email:password
+   * 1) adsklfjadlsfjkladsjklf;
+   * 2) email:password -> [email, password]
+   * 3) {email: eamil, password: password}
+   */
+  decodeBasicToken() {}
 }
