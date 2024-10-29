eb9eb41d9b7a0c7f906382a458ee76baeed06c1e
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-10-29 16:47:54 +0900

    controller 함수 리턴값 타입 추론 추가

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -10,33 +10,38 @@ import {
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostModel } from './entities/post.entity';
+import { DeleteResult } from 'typeorm';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
-  getPosts() {
+  getPosts(): Promise<PostModel[]> {
     return this.postsService.getAllPosts();
   }
 
   @Get(':id')
-  getPost(@Param('id', ParseIntPipe) id: number) {
+  getPost(@Param('id', ParseIntPipe) id: number): Promise<PostModel> {
     return this.postsService.getPostById(id);
   }
 
   @Post()
-  createPosts(createPostDto: Pick<PostModel, 'id' | 'title' | 'content'>) {
+  createPosts(
+    createPostDto: Pick<PostModel, 'id' | 'title' | 'content'>,
+  ): Promise<PostModel> {
     return this.postsService.createPost(createPostDto);
   }
 
   @Patch(':id')
-  patchPost(UpdatePostDto: Pick<PostModel, 'id' | 'title' | 'content'>) {
+  patchPost(
+    UpdatePostDto: Pick<PostModel, 'id' | 'title' | 'content'>,
+  ): Promise<PostModel> {
     return this.postsService.updatePost(UpdatePostDto);
   }
 
   @Delete(':id')
-  deletePost(@Param(':id', ParseIntPipe) id: number) {
+  deletePost(@Param(':id', ParseIntPipe) id: number): Promise<DeleteResult> {
     return this.postsService.deletePost(id);
   }
 }
