0188632b6ef8a118e0fb314c6bdf21334775fc48
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-11-11 16:06:28 +0900

    미사용 파일 삭제, recommendations 로직 백업

--- src/recommendation/entities/recommendation.entity.ts
+++ src/recommendation/entities/recommendation.entity.ts
@@ -14,3 +14,28 @@ export class RecommendationModel extends BaseModel {
   // @ManyToOne(() => UsersModel, (user) => user.recommendations)
   // user: UsersModel;
 }
+
+// import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn } from 'typeorm';
+// import { BaseModel } from '../../common/entities/base.entity';
+// import { UsersModel } from 'src/users/entities/users.entity';
+
+// @Entity()
+// export class FashionRecommendation extends BaseModel {
+//   @ManyToOne(() => UsersModel)
+//   user: UsersModel;
+
+//   @Column()
+//   summary: string;
+
+//   @Column('text')
+//   details: string;
+
+//   @Column('simple-array')
+//   keywords: string[];
+
+//   @Column('simple-array')
+//   related: string[];
+
+//   @Column('json')
+//   inputCriteria: Record<string, any>;
+// }
